8478473ca4d2c13e80ebf5b8ffea0096065cd4b4
emilianoPiz <emilianopizzuti95@gmail.com>
2024-07-16 11:11:07 +0200

    minor fix and add more boilerplate components

---
+++ src/app/website/shop/shop-promotions-banner/shop-promotions-banner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-promotions-banner',
+  standalone: true,
+  imports: [],
+  templateUrl: './shop-promotions-banner.component.html',
+  styleUrl: './shop-promotions-banner.component.scss'
+})
+export class ShopPromotionsBannerComponent {
+
+}
---
+++ src/app/website/shop/shop-reviews/shop-reviews.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-reviews',
+  standalone: true,
+  imports: [],
+  templateUrl: './shop-reviews.component.html',
+  styleUrl: './shop-reviews.component.scss'
+})
+export class ShopReviewsComponent {
+
+}
